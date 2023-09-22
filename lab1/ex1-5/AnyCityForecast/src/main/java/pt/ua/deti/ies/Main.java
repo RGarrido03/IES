23ae6d1d544f611684b25eb83badcebe5f537894
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-09-22 17:58:24 +0100

    Add city random list, fix bug

--- lab1/ex1-5/AnyCityForecast/src/main/java/pt/ua/deti/ies/Main.java
+++ lab1/ex1-5/AnyCityForecast/src/main/java/pt/ua/deti/ies/Main.java
@@ -6,48 +6,41 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.TimeUnit;
 
-// TODO: Add city list
 public class Main {
     private static final Logger logger = LogManager.getLogger(WeatherStarter.class);
 
-    public Main() {
+    private Main() {
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new AnyCityForecast(), 0, //initial delay
                                   20 * 1000); //subsequent rate
     }
 
-
     static class AnyCityForecast extends TimerTask {
         public void run() {
-            List<Integer> cityIdsList = List.of(1010500, 1010500);
+            List<Integer> cityIdsList =
+                    List.of(1010500, 1020500, 1030300, 1040200, 1050200, 1060300, 1070500, 1080500, 1081505, 1090700,
+                            1100900, 1110600, 1121400, 1131200, 1141600, 1160900, 1171400, 1182300);
 
-            while (true) {
-                try {
-                    int CITY_ID = cityIdsList.get((int) ((Math.random() * (cityIdsList.size()))));
-                    var forecast = WeatherStarter.getWeather(CITY_ID);
-                    if (forecast == null) throw new IOException("No results for this request");
-                    logger.info(String.format("""
-                                                      Forecast for city %d at %s
-                                                      Max temperature: %4.1f
-                                                      Min temperature: %4.1f
-                                                      Rain probability: %4.1f
-                                                      Wind: %d %s""",
-                                              CITY_ID,
-                                              forecast.getForecastDate(),
-                                              Double.parseDouble(forecast.getTMax()),
-                                              Double.parseDouble(forecast.getTMin()),
-                                              Double.parseDouble(forecast.getPrecipitaProb()),
-                                              forecast.getClassWindSpeed(),
-                                              forecast.getPredWindDir()));
-                    TimeUnit.SECONDS.sleep(20);
-                } catch (IOException e) {
-                    logger.error(e.getMessage());
-                } catch (InterruptedException e) {
-                    logger.error("Interrupted sleep! Exiting...");
-                    System.exit(1);
-                }
+            try {
+                int CITY_ID = cityIdsList.get((int) ((Math.random() * (cityIdsList.size()))));
+                var forecast = WeatherStarter.getWeather(CITY_ID);
+                if (forecast == null) throw new IOException("No results for this request");
+                logger.info(String.format("""
+                                                  Forecast for city %d
+                                                  Max temperature: %4.1f
+                                                  Min temperature: %4.1f
+                                                  Rain probability: %4.1f
+                                                  Wind: %d %s""",
+                                          CITY_ID,
+                                          Double.parseDouble(forecast.getTMax()),
+                                          Double.parseDouble(forecast.getTMin()),
+                                          Double.parseDouble(forecast.getPrecipitaProb()),
+                                          forecast.getClassWindSpeed(),
+                                          forecast.getPredWindDir()));
+            } catch (IOException e) {
+                logger.error(e.getMessage());
+                System.exit(1);
             }
         }
     }
