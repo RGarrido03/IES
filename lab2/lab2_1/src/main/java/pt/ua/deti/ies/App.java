3326486f6cff1203b710f39938fecb07e5e14498
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-09-28 11:38:28 +0100

    Initial files, ex 2.1

---
+++ lab2/lab2_1/src/main/java/pt/ua/deti/ies/App.java
@@ -0,0 +1,44 @@
+package pt.ua.deti.ies;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletHandler;
+
+import java.io.IOException;
+
+public class App
+{
+    private static final Logger logger = LogManager.getLogger(App.class);
+
+    public static void main(String[] args) throws Exception {
+
+        Server server = new Server(8680);
+
+        ServletHandler servletHandler = new ServletHandler();
+        server.setHandler(servletHandler);
+
+        servletHandler.addServletWithMapping(HelloServlet.class, "/");
+
+        server.start();
+        server.join();
+
+    }
+
+    public static class HelloServlet extends HttpServlet
+    {
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, IOException {
+            response.setContentType("text/html");
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getWriter().println("<h1>New Hello Simple Servlet</h1>");
+        }
+    }
+}
