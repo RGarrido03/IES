9adf8285e92b0f14f06bf187f40131605e441101
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-11-02 01:13:29 +0000

    Exercise 3.2 a) and b)

---
+++ lab3/ex2/src/main/java/pt/ua/deti/ies/ex2/Employee.java
@@ -0,0 +1,6 @@
+package pt.ua.deti.ies.ex2;
+
+import jakarta.persistence.*;
+
+public class Employee {
+}
---
+++ lab3/ex2/src/test/java/pt/ua/deti/ies/ex2/Ex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.ua.deti.ies.ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
